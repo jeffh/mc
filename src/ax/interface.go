a25c7d4cef338a5ca7c669216a390f6c586a0dde
Jeff Hui <jeff@jeffhui.net>
2013-09-06 00:41:08 -0700

    docstrings

--- src/ax/interface.go
+++ src/ax/interface.go
@@ -1,28 +1,39 @@
 package ax
 
-var DefaultLogger Logger = &NullLogger{}
+import (
+	"fmt"
+)
 
+// A NullLogger instance. A quick way to access the NullLogger
+var nullLogger Logger = &NullLogger{}
+
+// The core interface for all logging
 type Logger interface {
 	Printf(format string, values ...interface{})
 }
 
+// The interface for a logger that can wrap another logger
 type WrapLogger interface {
 	Logger
 	SetLogger(l Logger)
 	WrappedLogger() Logger
 }
 
-func UseOrDefault(logger Logger, defaultLogger Logger) Logger {
-	if logger == nil {
-		return defaultLogger
+// Shorthand for ensuring a logger is returned
+// You can provide as many fallback loggers as you want.
+// If all loggers are nil, the ax.nullLogger logger is used.
+func Use(loggers ...Logger) Logger {
+	for _, logger := range loggers {
+		if logger != nil {
+			return logger
+		}
 	}
-	return logger
+	return nullLogger
 }
 
-func Use(logger Logger) Logger {
-	return UseOrDefault(logger, DefaultLogger)
-}
-
+// Shorthand for wrapping a logger with WrapLoggers.
+// Multiple WrapLoggers can be provided to create a nested behavior
+// with the inner-most WrapLogger placed before the all other WrapLoggers.
 func Wrap(logger Logger, outerLoggers ...WrapLogger) WrapLogger {
 	previousLogger := logger
 	for _, outerLogger := range outerLoggers {
--- src/ax/wrapped.go
+++ src/ax/wrapped.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// Basic embeddable Logger type that can accept another logger to wrap
 type BasicWrappedLogger struct {
 	Logger Logger
 }
@@ -22,6 +23,9 @@ func (l *BasicWrappedLogger) WrappedLogger() Logger {
 	return l.Logger
 }
 
+// A WrapLogger that ensures only one message is being logged to the
+// wrapped logger at a time. This is done by locking before each call
+// to the wrapped logger
 type LockedLogger struct {
 	BasicWrappedLogger
 	mutex sync.Mutex
@@ -37,7 +41,7 @@ func (l *LockedLogger) Printf(format string, v ...interface{}) {
 	l.Logger.Printf(format, v...)
 }
 
-// A Logger that prefixes the logger it has been given
+// A Logger that prefixes the log messages it has been given
 // with a given string.
 type PrefixLogger struct {
 	BasicWrappedLogger
@@ -53,6 +57,8 @@ func (l *PrefixLogger) Printf(format string, v ...interface{}) {
 	l.Logger.Printf("%s%s", l.Prefix, output)
 }
 
+// A WrapLogger that logs messages with the current time prefixed
+// in front of it
 type TimestampLogger struct {
 	BasicWrappedLogger
 	Now func() string
